37d020919f63077763c26ed73d4e19a7f39c49ba
Samuel-Brito19 <sambriton7@gmail.com>
2024-02-01 14:06:51 -0300

    Studying scanf function

--- Aulas/variables.cpp
+++ Aulas/variables.cpp
@@ -6,5 +6,11 @@ int main()
 {
 
     setlocale(LC_ALL, "");
-    printf("Olá mundo!");
+    printf("Olá \n");
+
+    int a = 5;
+
+    printf(" O valor a de é: %d", a);
+    scanf("%d", &a);
+    printf("O valor de a mudou para %d", a);
 }
