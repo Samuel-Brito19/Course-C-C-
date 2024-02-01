24955f46d528fd697b94b24b2f4f78d8fd453606
Samuel-Brito19 <sambriton7@gmail.com>
2024-02-01 14:58:19 -0300

    Float and char variables

--- Aulas/variables.cpp
+++ Aulas/variables.cpp
@@ -10,7 +10,23 @@ int main()
 
     int a = 5;
 
-    printf(" O valor a de é: %d", a);
+    printf(" O valor a de é: %d \n", a);
     scanf("%d", &a);
     printf("O valor de a mudou para %d", a);
+
+    float b = 5.6;
+
+    printf(" O valor b de é: %f \n", b);
+    scanf("%f", &b);
+    printf("O valor de b mudou para %f", b);
+
+    char d = 't';
+
+    printf("digite um caractere:\n");
+
+    fflush(stdin);
+
+    scanf(" %c", &d); // aqui adicionei um espaço
+
+    printf("O caractere d mudou para: %c\n", d);
 }
