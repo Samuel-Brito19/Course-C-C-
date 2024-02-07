03ebc255e66042b59506a76de05c2de56feeab71
Samuel-Brito19 <sambriton7@gmail.com>
2024-02-07 10:56:39 -0300

    Basic logic operators

---
+++ Aulas/multiplesConditionals.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = ;
+
+    if (a > 5 && a < 15)
+    {
+        printf("Variable between 10 and 15");
+    }
+    else if (a == 10 || a == 5)
+    {
+        printf("\n number is either 10 or 5");
+    }
+    else if (15 < a < 5 || a == 20)
+    {
+        printf("\n Variable beteween 15 and 5 or is 20");
+    }
+}
